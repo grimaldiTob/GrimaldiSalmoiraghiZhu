848faca09e843efc65ea0fd9df487fe5d9f7650a
Zumike <donghua.zhu@mail.polimi.it>
2026-05-13 18:53:59 +0200

    remove unused method of BatchAccumulatorInterface

--- src/interfaces/BatchAccumulatorInterface.h
+++ src/interfaces/BatchAccumulatorInterface.h
@@ -15,6 +15,5 @@ public:
     // ok so hypotetically this method receives a telemetry batch, which was populated by the ingestor
     // it is more efficient to use references so that we can just erase the content of the batch and use it again.
     virtual void storeValidData(TelemetryBatch&) = 0;
-
-    virtual void sortPriorities() = 0;
+    
 };
